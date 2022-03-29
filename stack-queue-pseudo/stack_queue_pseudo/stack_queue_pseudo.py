ae33205a96b6253e0d2d0205b108af3bbc460537
Barham Farraj <barhamfarraj@icloud.com>
2022-03-29 18:39:17 +0300

    done

--- stack-queue-pseudo/stack_queue_pseudo/stack_queue_pseudo.py
+++ stack-queue-pseudo/stack_queue_pseudo/stack_queue_pseudo.py
@@ -1,22 +1,32 @@
-class Queue:
+# from Stacks_and_Queues.stacks_and_queues import Queue , Stack
+class psuedoQueue:
+    """
+    Implements a Queue using two Stacks.
+    """
+
     def __init__(self):
         self.s1 = []
         self.s2 = []
  
     def enqueue(self, x):
+        """
+        Inserts value into the PseudoQueue, using a first-in, first-out approach.
+        """
     
         while len(self.s1):
             self.s2.append(self.s1[-1])
             self.s1.pop()
- 
+             
         self.s1.append(x)
  
-        
         while len(self.s2):
             self.s1.append(self.s2[-1])
             self.s2.pop()
  
     def dequeue(self):
+        """
+        Extracts a value from the PseudoQueue, using a first-in, first-out approach.
+        """
          
         if len(self.s1) == 0:
             print("Q is Empty")
@@ -27,11 +37,11 @@ class Queue:
 
  
 if __name__ == '__main__':
-    q = Queue()
-    q.enqueue('hi')
+    q = psuedoQueue()
+    print(q.enqueue('hi'))
     q.enqueue('there')
     q.enqueue('!')
  
-    print(q.dequeue())
-    print(q.dequeue())
-    print(q.dequeue())    
+    # print(q.dequeue())
+    # print(q.dequeue())
+    # print(q.dequeue())    
--- stack-queue-pseudo/tests/test_stack_queue_pseudo.py
+++ stack-queue-pseudo/tests/test_stack_queue_pseudo.py
@@ -1,5 +1,23 @@
-from stack_queue_pseudo import __version__
+from stack_queue_pseudo.stack_queue_pseudo import psuedoQueue
+import pytest
+
+def test_enqueue():
+    q = psuedoQueue()
+    q.enqueue('Me')
+    assert q.dequeue() == "Me"
+
+def test_dequeue(my_pseudo_queue):
+    assert my_pseudo_queue.dequeue() == "Hi"
+
+def test_dequeue_empty():
+    with pytest.raises(Exception):
+        psuedoQueue().dequeue()
 
 
-def test_version():
-    assert __version__ == '0.1.0'
+
+
+@pytest.fixture
+def my_pseudo_queue():
+    q = psuedoQueue()
+    [q.enqueue(i) for i in ["Hi", "There", "!"]]
+    return q
